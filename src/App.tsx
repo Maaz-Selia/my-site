4a35fac407978a2db92cd59697219fc19681b0ab
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-02-10 11:21:36 +0000

    Assigned bg colours to Grid Items for better visualisation

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,23 @@ import { Grid, GridItem } from "@chakra-ui/react"
 function App() {
 
   return (
-    <Grid h='100%'
+    <Grid h='100vh'
           templateColumns='repeat(14, 1fr)'
           templateRows='repeat(14, 1fr)'
           gap={4}
     >
-      <GridItem colStart={2} colSpan={1} rowStart={2} rowSpan={12}>
+      <GridItem colStart={2} colSpan={1} rowStart={2} rowSpan={12} bg='#FAEBD7'>
         NavBar
       </GridItem>
-      <GridItem colStart={3} colSpan={8} rowStart={5} rowSpan={1}>
+      <GridItem colStart={3} colSpan={8} rowStart={5} rowSpan={1} bg='#FAEBD7'>
         LinkBar
       </GridItem>
-      <GridItem colStart={3} colSpan={9} rowStart={9} rowSpan={3}>
+      <GridItem colStart={3} colSpan={10} rowStart={9} rowSpan={3} bg='#FAEBD7'>
         TextBody
       </GridItem>
-      <GridItem colStart={7} colSpan={2} rowStart={13} rowSpan={1}>
+      <GridItem colStart={7} colSpan={2} rowStart={13} rowSpan={1} bg='#FAEBD7'>
         DownArrow
       </GridItem>
-      <GridItem></GridItem>
     </Grid>
   )
 }
