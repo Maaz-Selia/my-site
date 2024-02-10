66c4634b765fa9a5cd434a1052ac49c915c6d4eb
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-02-10 11:16:14 +0000

    Built Grid Layout for Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@chakra-ui/react"
-
+import { Grid, GridItem } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <Button>My Button</Button>
+    <Grid h='100%'
+          templateColumns='repeat(14, 1fr)'
+          templateRows='repeat(14, 1fr)'
+          gap={4}
+    >
+      <GridItem colStart={2} colSpan={1} rowStart={2} rowSpan={12}>
+        NavBar
+      </GridItem>
+      <GridItem colStart={3} colSpan={8} rowStart={5} rowSpan={1}>
+        LinkBar
+      </GridItem>
+      <GridItem colStart={3} colSpan={9} rowStart={9} rowSpan={3}>
+        TextBody
+      </GridItem>
+      <GridItem colStart={7} colSpan={2} rowStart={13} rowSpan={1}>
+        DownArrow
+      </GridItem>
+    </Grid>
   )
 }
 
