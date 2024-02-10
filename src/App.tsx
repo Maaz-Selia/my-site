17035bc01f77cd3f2ef4c2d1c7d9dfd841268044
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-02-10 12:00:39 +0000

    Extended Chakra Theme to include my theme colours

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Grid, GridItem, useColorModeValue } from "@chakra-ui/react"
 
 function App() {
 
+  //const bg = useColorModeValue('lightBrown', 'darkBrown');
+
   return (
+    <>
+    
     <Grid h='100vh'
+          
           templateColumns='repeat(14, 1fr)'
           templateRows='repeat(14, 1fr)'
-          gap={4}
+          gap={3}
     >
-      <GridItem colStart={2} colSpan={1} rowStart={2} rowSpan={12} bg='#FAEBD7'>
+      <GridItem colStart={2} colSpan={1} rowStart={2} rowSpan={12} bg='darkBrown'>
         NavBar
       </GridItem>
-      <GridItem colStart={3} colSpan={8} rowStart={5} rowSpan={1} bg='#FAEBD7'>
+      <GridItem colStart={3} colSpan={8} rowStart={5} rowSpan={1} bg='darkBrown'>
         LinkBar
       </GridItem>
-      <GridItem colStart={3} colSpan={10} rowStart={9} rowSpan={3} bg='#FAEBD7'>
+      <GridItem colStart={3} colSpan={10} rowStart={9} rowSpan={3} bg='darkBrown'>
         TextBody
       </GridItem>
-      <GridItem colStart={7} colSpan={2} rowStart={13} rowSpan={1} bg='#FAEBD7'>
+      <GridItem colStart={7} colSpan={2} rowStart={14} rowSpan={1} bg='darkBrown'>
         DownArrow
       </GridItem>
     </Grid>
+    <Grid h='100vh'></Grid>
+    </>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    colors: {
+        darkBrown:'#716758',
+        lightBrown: '#E9DCD6',
+        altBrown: '#D9C9C1'
+
+    }
+});
+
+export default theme;
