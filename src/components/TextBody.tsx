8049f70ae7f64025ccb3957ee2ad39b20f3bf819
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-03-26 13:55:00 +0000

    Adjusted text size of TextBody

--- src/components/TextBody.tsx
+++ src/components/TextBody.tsx
@@ -3,8 +3,9 @@ import { Heading, Text } from '@chakra-ui/react'
 const Body = () => {
   return (
     <>
-      <Heading>Maaz Selia</Heading>
-      <Text>I love to code; ride motorbikes, play guitar, read, and spend time with loved ones.</Text>
+      <Heading as='i' fontSize={'6xl'}>Maaz Selia</Heading>
+      <br />
+      <Text as='i' fontSize={'3xl'}>I love to code; ride motorbikes, play guitar, read, and spend time with loved ones.</Text>
     </>
   )
 }
