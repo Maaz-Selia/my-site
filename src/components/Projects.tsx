d1db991abec47b8d42869aed872f385b9b5484e8
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-04-16 13:26:47 +0100

    Created projects Grid component and added some styling t projects  title

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,13 @@
 import { GridItem, Text } from "@chakra-ui/react"
+import ProjectsGrid from "./ProjectsGrid"
 
 const Projects = () => {
   return (
     <>
-        <GridItem colStart={4} colSpan={8} rowStart={2} rowSpan={1}>
-            <Text fontSize='5xl'>// Projects</Text>
+        <GridItem colStart={4} colSpan={8} rowStart={2} rowSpan={1} alignItems='center'>
+            <Text as='i' fontSize='5xl' opacity='20%' textAlign='center' letterSpacing='4vw'>// PROJECTS</Text>
         </GridItem>
+        <ProjectsGrid />
     </>
   )
 }
