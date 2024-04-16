2c340b18445247e95bfc5e92a487d206ed6fe1de
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-04-16 15:20:42 +0100

    Added 'ID' to ProjectsCard and some styling

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -3,8 +3,8 @@ import { Card, CardBody, CardHeader, Text } from "@chakra-ui/react"
 const ProjectsCard = () => {
   return (
     <Card width='100%' height='100%' borderRadius='0'>
-        <CardHeader bgColor='var(--chakra-colors-chakra-body-text)' borderTopRadius='5'>
-            <Text as='i' color='var(--chakra-colors-white)'>ID:</Text>
+        <CardHeader bgColor='var(--chakra-colors-chakra-body-text)' borderTopRadius='5' padding='2'>
+            <Text as='i' fontSize='2xl' color='var(--chakra-colors-white)'>ID:</Text>
         </CardHeader>
         <CardBody></CardBody>
     </Card>
