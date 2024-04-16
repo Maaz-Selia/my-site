7e13f13efdbb8548f449b21c66f1eb00ce4c7166
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-04-16 14:41:37 +0100

    Added basic structure to ProjectsCard component

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, CardHeader, Text } from "@chakra-ui/react"
 
 const ProjectsCard = () => {
   return (
-    <Card>
+    <Card width='100%' height='100%' borderRadius='10' bgColor='var(--chakra-colors-chakra-body-text)'>
         <CardHeader></CardHeader>
         <CardBody></CardBody>
     </Card>
--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -6,16 +6,16 @@ const ProjectsGrid = () => {
 
   return (
     <>
-        <GridItem colStart={4} colSpan={3} rowStart={4} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+        <GridItem colStart={4} colSpan={3} rowStart={4} rowSpan={4}>
             <ProjectsCard />
         </GridItem>
-        <GridItem colStart={9} colSpan={3} rowStart={4} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+        <GridItem colStart={9} colSpan={3} rowStart={4} rowSpan={4}>
             <ProjectsCard />
         </GridItem>
-        <GridItem colStart={4} colSpan={3} rowStart={9} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+        <GridItem colStart={4} colSpan={3} rowStart={9} rowSpan={4}>
             <ProjectsCard />
         </GridItem>
-        <GridItem colStart={9} colSpan={3} rowStart={9} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+        <GridItem colStart={9} colSpan={3} rowStart={9} rowSpan={4}>
             <ProjectsCard />
         </GridItem>
     </>
