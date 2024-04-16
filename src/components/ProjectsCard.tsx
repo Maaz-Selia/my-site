c73063085a8d7d6383e4fabe2dec4006f9267169
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-04-16 13:45:43 +0100

    Created ProjectsCard component

---
+++ src/components/ProjectsCard.tsx
@@ -0,0 +1,12 @@
+import { Card, CardBody, CardHeader, Text } from "@chakra-ui/react"
+
+const ProjectsCard = () => {
+  return (
+    <Card>
+        <CardHeader></CardHeader>
+        <CardBody></CardBody>
+    </Card>
+  )
+}
+
+export default ProjectsCard
--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -1,8 +1,24 @@
+import { GridItem, Text } from '@chakra-ui/react'
 import React from 'react'
+import ProjectsCard from './ProjectsCard'
 
 const ProjectsGrid = () => {
+
   return (
-    <div>ProjectsGrid</div>
+    <>
+        <GridItem colStart={4} colSpan={3} rowStart={4} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+            <ProjectsCard />
+        </GridItem>
+        <GridItem colStart={9} colSpan={3} rowStart={4} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+            <ProjectsCard />
+        </GridItem>
+        <GridItem colStart={4} colSpan={3} rowStart={9} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+            <ProjectsCard />
+        </GridItem>
+        <GridItem colStart={9} colSpan={3} rowStart={9} rowSpan={4} bgColor='var(--chakra-colors-chakra-body-text)'>
+            <ProjectsCard />
+        </GridItem>
+    </>
   )
 }
 
