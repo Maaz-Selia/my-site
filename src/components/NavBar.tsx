0b43e6c9216af9b9c361568f7749cf99502adf53
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-04-16 10:32:35 +0100

    Added view switch to all NavBar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import { VStack, Text } from '@chakra-ui/react'
 import Line from './Line';
 import '../index.css'
 import Contact from './Contact';
+import Interests from './Interests';
+import Projects from './Projects';
 
 interface Props {
 
@@ -17,8 +19,8 @@ const NavBar = ( {setCurrentView }: Props) => {
         <VStack maxHeight='100%' gap={10}>
             <Line size='10vh' orientation='v' />
             <Text size={fontSize} className='vertical-text' onClick={() => setCurrentView(<Contact />)}>Contact</Text>
-            <Text size={fontSize} className='vertical-text' onClick={() => console.log('clicked Interests')}>Interests</Text>
-            <Text size={fontSize} className='vertical-text' onClick={() => console.log('clicked Projects')}>Projects</Text>
+            <Text size={fontSize} className='vertical-text' onClick={() => setCurrentView(<Interests />)}>Interests</Text>
+            <Text size={fontSize} className='vertical-text' onClick={() => setCurrentView(<Projects />)}>Projects</Text>
             <Line size='40vh' orientation='v' />
         </VStack>
     )
