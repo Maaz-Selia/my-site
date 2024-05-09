2582b129e75822b1ba30e720fc9fbf64f3d2c175
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-05-09 10:58:27 +0100

    Formatted projectcard header

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -3,8 +3,8 @@ import { Card, CardBody, CardHeader, Text } from "@chakra-ui/react"
 const ProjectsCard = () => {
   return (
     <Card width='100%' height='100%' borderRadius='0'>
-        <CardHeader bgColor='var(--chakra-colors-chakra-body-text)' borderTopRadius='5' padding='2'>
-            <Text as='i' fontSize='2xl' color='var(--chakra-colors-white)'>ID:</Text>
+        <CardHeader bgColor='var(--chakra-colors-chakra-body-text)' borderTopRadius='5' padding='2' textAlign='center'>
+            <Text as='i' fontSize='2xl' color='var(--chakra-colors-chakra-body-bg)' letterSpacing='0.3vw'>On the way!</Text>
         </CardHeader>
         <CardBody></CardBody>
     </Card>
